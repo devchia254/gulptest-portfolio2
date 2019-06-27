57c7346488b18ce50bb6c52b1a5c66de3381a29f
devchia254 <alex7chiaster@gmail.com>
2019-06-27 09:40:04 +0800

    testing scrollreveal.js and gulp-concat

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,10 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.cover-title');
+ScrollReveal().reveal('#know-more', { delay: 500 });
+ScrollReveal().reveal('.section-title', { delay: 200 });
+ScrollReveal().reveal('.img-frame', { delay: 500 });
+ScrollReveal().reveal('.content', { delay: 500 });
+ScrollReveal().reveal('.proj-desc', { delay: 500 });
+ScrollReveal().reveal('.proj-img', { delay: 500 });
+
+
--- dist/all.js
+++ dist/all.js
@@ -94,3 +94,14 @@ window.onload = function() {
 
 }
 
+
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.cover-title');
+ScrollReveal().reveal('#know-more', { delay: 500 });
+ScrollReveal().reveal('.section-title', { delay: 200 });
+ScrollReveal().reveal('.img-frame', { delay: 500 });
+ScrollReveal().reveal('.content', { delay: 500 });
+ScrollReveal().reveal('.proj-desc', { delay: 500 });
+ScrollReveal().reveal('.proj-img', { delay: 500 });
+
+
